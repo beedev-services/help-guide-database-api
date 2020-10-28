66b5f934217cec70903066facb27a1a48c20650e
Melissa Tanksley <melissa.tanksley@gmail.com>
2020-10-28 15:32:18 -0700

    building db-configs; added functions

--- database/db-config.js
+++ database/db-config.js
@@ -1,6 +1,41 @@
-require("dotenv").config();
 const knex = require("knex");
+
+require("dotenv").config();
 
 const config = require("../knexfile.js");
 
-module.exports = knex(config[process.env.NODE_ENV || "development"]);
+
+module.exports = {
+    find,
+    findById,
+    insert,
+    update,
+    remove,
+  };
+  
+  function find() {
+    return db('guides');
+  }
+  
+  function findById(id) {
+    return db('guides').where({ id: Number(id) });
+  }
+  
+  function insert(guide) {
+    return db('guides')
+      .insert(guide, 'id')
+      .then(ids => ({ id: ids[0] }));
+  }
+  
+  function update(id, guide) {
+    return db('guides')
+      .where('id', Number(id))
+      .update(guide);
+  }
+  
+  function remove(id) {
+    return db('guides')
+      .where('id', Number(id))
+      .del();
+  }
+
